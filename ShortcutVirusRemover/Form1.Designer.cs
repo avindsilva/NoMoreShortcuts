11d331634e571f38020229bcc0420fc6c8dc5c15
Avin D'Silva <avindsilva@gmail.com>
2014-03-14 00:56:02 +0530

    Added all project files

---
+++ ShortcutVirusRemover/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace ShortcutVirusRemover
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.AllowDrop = true;
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.ItemSize = new System.Drawing.Size(80, 18);
+            this.tabControl1.Location = new System.Drawing.Point(2, 3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(431, 248);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.radioButton2);
+            this.tabPage1.Controls.Add(this.radioButton1);
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(423, 222);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Be Virus Free";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(331, 60);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(60, 20);
+            this.button3.TabIndex = 27;
+            this.button3.Text = "&Refresh";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Select a Folder";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "Select a Drive";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(38, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(353, 19);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Select a drive or a folder to remove the shortcut virus";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(112, 120);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(14, 13);
+            this.radioButton2.TabIndex = 23;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged_1);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(112, 62);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(14, 13);
+            this.radioButton1.TabIndex = 22;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged_1);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(132, 59);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(193, 21);
+            this.comboBox1.TabIndex = 21;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(331, 119);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(60, 21);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "&Browse...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(132, 120);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(193, 21);
+            this.textBox1.TabIndex = 18;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(121, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 26);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "&Remove Shortcut Virus\r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(423, 222);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "About";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(93, 4);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(215, 216);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Developers\r\nAvin D\'Silva\r\nDarshan Adiga\r\nManel Ramesh Nayak\r\n\r\nAn initiative of \r" +
+                "\nhttp://www.whatnotwhynot.com\r\n\r\nIssued in Public Interest by \r\nTeam BugAllergy\r" +
+                "\n\r\nNo More Shortcuts (Beta)\r\n";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.textBox2);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(423, 222);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Help";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.AllowDrop = true;
+            this.textBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBox2.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.textBox2.Location = new System.Drawing.Point(3, 2);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(417, 218);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.Text = resources.GetString("textBox2.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 251);
+            this.Controls.Add(this.tabControl1);
+            this.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "No More Shortcuts (Beta)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+
+
+    }
+}
+
---
+++ ShortcutVirusRemover/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ShortcutVirusRemover
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loadDrives()
+        {
+            comboBox1.Items.Clear();
+            int i = 0;
+            string drive;
+            try
+            {
+                DriveInfo[] di = DriveInfo.GetDrives();
+
+                foreach (string drives in Environment.GetLogicalDrives())
+                {
+                    comboBox1.Items.Insert(i++,drives);
+                }
+            }
+            catch (DriveNotFoundException d)
+            {
+                Console.WriteLine("Message : {0}", d.Message);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Message : {0}", ex.Message);
+            }
+                
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadDrives();          
+        }
+
+       private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+       {
+            
+       }
+
+       private void button2_Click(object sender, EventArgs e)
+       {
+           FolderBrowserDialog x = new FolderBrowserDialog();
+           x.ShowDialog();
+           string selected_path = x.SelectedPath;
+           textBox1.Text = selected_path;
+       }
+
+       private void button1_Click(object sender, EventArgs e)
+       {
+           string command = null;
+           
+           if (radioButton1.Checked == true)
+               command = "attrib -S -H " + textBox1.Text + "*.*" + " /s /d";
+           if(radioButton2.Checked == true)
+              command = "attrib -S -H " + textBox1.Text + " /s /d";
+
+           System.Diagnostics.ProcessStartInfo procStart = new System.Diagnostics.ProcessStartInfo("cmd", "/c " + command);
+           //procStart.CreateNoWindow = true;
+           MessageBox.Show("Shortcut Virus removed!");
+
+       }
+
+       private void button3_Click(object sender, EventArgs e)
+        {
+            loadDrives();
+        }
+
+       private void radioButton1_CheckedChanged_1(object sender, EventArgs e)
+       {
+           comboBox1.Visible = true;
+           button3.Visible = true;
+
+           textBox1.Visible = false;
+           button2.Visible = false;
+
+       }
+
+       private void radioButton2_CheckedChanged_1(object sender, EventArgs e)
+       {
+           comboBox1.Visible = false;
+           button3.Visible = false;
+
+           textBox1.Visible = true;
+           button2.Visible = true;
+       }
+
+       private void button1_Click_1(object sender, EventArgs e)
+       {
+           string command = null;
+
+           if (radioButton1.Checked == true)
+               command = "-S -H " + comboBox1.Text + "*.*" + " /s /d";
+           if (radioButton2.Checked == true)
+               command = "-S -H " + textBox1.Text + " /s /d";
+           
+           Console.WriteLine(command);
+
+           //System.Diagnostics.Process process = new System.Diagnostics.Process();
+           //System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+           //startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+           //startInfo.FileName = "attrib.exe";
+           //startInfo.Arguments = command;
+           //process.StartInfo = startInfo;
+           //process.Start();
+
+           System.Diagnostics.Process.Start("attrib.exe", command);
+           MessageBox.Show("Congratulations! The Shortcut Virus has been removed.\n Your will find your files in folder without a name on your device. ");
+
+       }
+
+       private void button2_Click_1(object sender, EventArgs e)
+       {
+           FolderBrowserDialog x = new FolderBrowserDialog();
+           x.ShowDialog();
+           string selected_path = x.SelectedPath;
+           textBox1.Text = selected_path;
+       }
+
+       private void label3_Click(object sender, EventArgs e)
+       {
+
+       }
+      
+    }
+}
